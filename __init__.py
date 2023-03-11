8ec2b8a3fa9ae6b09964b2e389a91ceefa233841
Hacker 17082006 <tai17082006.tk@gmail.com>
2023-03-11 16:16:33 +0700

    Fix path bug

--- __init__.py
+++ __init__.py
@@ -5,10 +5,11 @@ except:
     safetensors_support = False
 import torch
 import os
+from pathlib import Path
 
 class SaveCheckpoint:
     def __init__(self):
-        self.output_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "../models/checkpoint")
+        self.output_dir = Path(os.path.dirname(os.path.realpath(__file__)), "../../models/checkpoint")
 
     @classmethod
     def INPUT_TYPES(s):
@@ -35,10 +36,10 @@ class SaveCheckpoint:
         state_dict = {**model_state_dict, **clip_state_dict, **vae_state_dict}
         if save_as == ".ckpt":
             filename = f"{name}.ckpt"
-            torch.save({"state_dict": state_dict}, os.path.join(self.output_dir, filename))
+            torch.save({"state_dict": state_dict}, Path(self.output_dir, filename).resolve())
         else:
             filename = f"{name}.safetensors"
-            safetensors.torch.save_file({"state_dict": state_dict}, os.path.join(self.output_dir, filename))
+            safetensors.torch.save_file({"state_dict": state_dict}, Path(self.output_dir, filename).resolve())
 
         clip.patcher.unpatch_model()
         model.unpatch_model()
