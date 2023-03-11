aca55d2eaa70cd97e6daef5402c7c58540ff01e3
Hacker 17082006 <tai17082006.tk@gmail.com>
2023-03-11 22:37:33 +0700

    Finally :-:

--- __init__.py
+++ __init__.py
@@ -6,10 +6,13 @@ except:
 import torch
 import os
 from pathlib import Path
+import gc
+
 
 class SaveCheckpoint:
     def __init__(self):
-        self.output_dir = Path(os.path.dirname(os.path.realpath(__file__)), "../../models/checkpoints")
+        self.output_dir = Path(os.path.dirname(
+            os.path.realpath(__file__)), "../../models/checkpoints")
 
     @classmethod
     def INPUT_TYPES(s):
@@ -29,21 +32,35 @@ class SaveCheckpoint:
     CATEGORY = "checkpoint"
 
     def save_checkpoint(self, name, model, clip, vae, save_as):
-        model_state_dict = model.model.state_dict() #Yes. "model.model"
-        clip_state_dict = clip.patcher.patch_model().state_dict()
-        vae_state_dict = vae.first_stage_model.state_dict()
 
-        state_dict = {**model_state_dict, **clip_state_dict, **vae_state_dict}
+        state_dict_parts = {
+            # Yes. "model.model". This is actually the UNet lol
+            "model": model.model.state_dict(),
+            "cond_stage_model": clip.cond_stage_model.state_dict(),
+            "first_stage_model": vae.first_stage_model.state_dict()
+        }
+        state_dict = {}
+        for part_key, part_value in state_dict_parts.items():
+            for k, v in part_value.items():
+                state_dict[f"{part_key}.{k}"] = v
+
         if save_as == ".ckpt":
             filename = f"{name}.ckpt"
-            torch.save({"state_dict": state_dict}, Path(self.output_dir, filename).resolve())
+            torch.save({"state_dict": state_dict}, Path(
+                self.output_dir, filename).resolve())
         else:
             filename = f"{name}.safetensors"
-            safetensors.torch.save_file({"state_dict": state_dict}, Path(self.output_dir, filename).resolve())
+            safetensors.torch.save_file({"state_dict": state_dict}, Path(
+                self.output_dir, filename).resolve())
 
-        clip.patcher.unpatch_model()
-        model.unpatch_model()
-        return { "ui": { "model": filename} }
+        del state_dict
+        gc.collect()
+        if torch.cuda.is_available():
+            if torch.version.cuda:  # This seems to make things worse on ROCm so I only do it for cuda
+                torch.cuda.empty_cache()
+                torch.cuda.ipc_collect()
+        return {"ui": {"model": filename}}
+
 
 NODE_CLASS_MAPPINGS = {
     "SaveCheckpoint": SaveCheckpoint
