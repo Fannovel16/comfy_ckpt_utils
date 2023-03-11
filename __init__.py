b8f8494446d06d20e04a93829e5b6811ffa4cc52
Hacker 17082006 <tai17082006.tk@gmail.com>
2023-03-11 23:15:17 +0700

    Truely Finally?

--- __init__.py
+++ __init__.py
@@ -7,7 +7,7 @@ import torch
 import os
 from pathlib import Path
 import gc
-
+import model_management
 
 class SaveCheckpoint:
     def __init__(self):
@@ -34,12 +34,10 @@ class SaveCheckpoint:
     def save_checkpoint(self, name, model, clip, vae, save_as):
 
         state_dict_parts = {
-            # Yes. "model.model". This is actually the UNet lol
-            "model": model.model.state_dict(),
             "cond_stage_model": clip.cond_stage_model.state_dict(),
             "first_stage_model": vae.first_stage_model.state_dict()
         }
-        state_dict = {}
+        state_dict = {**model.model.state_dict()}
         for part_key, part_value in state_dict_parts.items():
             for k, v in part_value.items():
                 state_dict[f"{part_key}.{k}"] = v
@@ -54,6 +52,7 @@ class SaveCheckpoint:
                 self.output_dir, filename).resolve())
 
         del state_dict
+        model_management.unload_model()
         gc.collect()
         if torch.cuda.is_available():
             if torch.version.cuda:  # This seems to make things worse on ROCm so I only do it for cuda
